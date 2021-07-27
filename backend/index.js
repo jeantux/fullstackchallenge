c47818df71dc3569491e53d4a5b7136345338534
Jean Carlos <jeaanca.santos@gmail.com>
2021-07-27 20:54:55 -0300

    Allow parser of body content of request

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const routes = require('./routes')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
 app.get('/', (req, res) => {
   res.send('Server Ok!')
 })
