baa5c737e9c128bb650ae59e990603619b8872b2
Jean Carlos <jeaanca.santos@gmail.com>
2021-07-27 19:39:14 -0300

    Added new dependencies * Nodemon to run in dev mode * Knex to connect with database

---
+++ backend/knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgre',
+      password : 'postgres',
+      database : 'vehicle_test',
+      charset: 'utf8'
+    },
+    migrations: {
+      directory: __dirname + '/knex/migrations',
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
